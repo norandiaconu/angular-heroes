24a82f2502d3a25dc8fc6a1d50478c97b1ec804d
Noran Diaconu <ndiaconu@empoweredbenefits.com>
2020-01-06 14:59:50 -0500

    Added support for adding, deleting, and searching for heroes

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+
+@Component({
+  selector: "app-hero-search",
+  templateUrl: "./hero-search.component.html",
+  styleUrls: ["./hero-search.component.css"]
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private readonly searchTerms = new Subject<string>();
+  searchWait = 300;
+
+  constructor(private readonly heroService: HeroService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(this.searchWait), distinctUntilChanged(), switchMap(
+        (term: string) => this.heroService.searchHeroes(term)
+      )
+    );
+  }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-	
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-  };
   
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
-
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(private readonly heroService: HeroService) { }
 
   getHeroes(): void {
-    //this.heroes = this.heroService.getHeroes();
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {this.heroes.push(hero);});
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
   ngOnInit() {
