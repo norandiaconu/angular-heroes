aa626de25d7743920d453a375e39ea7589dc02da
Noran Diaconu <norandiaconu@gmail.com>
2026-03-02 19:14:42 -0500

    Updated to Angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
-import { enableProdMode, importProvidersFrom } from "@angular/core";
-import { environment } from "./environments/environment";
-import {
-    provideHttpClient,
-    withInterceptorsFromDi,
-} from "@angular/common/http";
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataService } from "./app/in-memory-data.service";
-import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app/app-routing.module";
-import { AppComponent } from "./app/app.component";
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { environment } from './environments/environment';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './app/in-memory-data.service';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
     enableProdMode();
@@ -17,12 +14,13 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
+        provideZoneChangeDetection(),
         importProvidersFrom(BrowserModule, FormsModule, AppRoutingModule),
         provideHttpClient(withInterceptorsFromDi()),
         importProvidersFrom([
             HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-                dataEncapsulation: false,
-            }),
-        ]),
-    ],
+                dataEncapsulation: false
+            })
+        ])
+    ]
 }).catch((err) => console.error(err));
